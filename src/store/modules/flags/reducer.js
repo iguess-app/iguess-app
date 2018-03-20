477a4e56154e62df2c28ffa4fa9b2c715cd6d420
Julio Xavier <julioxavierr@gmail.com>
2018-03-19 22:09:47 -0300

    Using Immutable on flags and actionTypes file

--- src/store/modules/flags/reducer.js
+++ src/store/modules/flags/reducer.js
@@ -1,33 +1,33 @@
 /* @flow */
 
-const NOTIFICATION = 'SHOW_NOTIFICATION'
-const ACTIVE_SCREEN_SWIPER = 'ACTIVE_SCREEN_SWIPER'
+import Immutable from 'seamless-immutable';
+import * as types from './actionTypes';
 
-const defaultFlags = {
+const initialState = Immutable({
   notification: false,
   activeSwiperScreen: 1
-}
+});
 
 type Action = Object<{ type: string, payload: bool }>
 type Flags = Object<{ notification: bool, activeSwiperScreen: number }>
 
-export default function flags(state: Flags = defaultFlags, action: Action): Flags {
+export default function reduce(state: Flags = initialState, action: Action = {}): Flags {
   switch (action.type) {
-  case NOTIFICATION:
-    return Object.assign({}, state, { notification: !state.notification })
-  case ACTIVE_SCREEN_SWIPER:
-    return Object.assign({}, state, { activeSwiperScreen: action.payload })
-  default:
-    return state
+    case types.NOTIFICATION:
+      return Object.assign({}, state, { notification: !state.notification });
+    case types.ACTIVE_SCREEN_SWIPER:
+      return Object.assign({}, state, { activeSwiperScreen: action.payload });
+    default:
+      return state;
   }
 }
 
 export const changeSwiperChange = (payload): Action => ({
-  type: ACTIVE_SCREEN_SWIPER,
-  payload
+    type: types.ACTIVE_SCREEN_SWIPER,
+    payload
 })
-
+  
 export const showNotification = (payload): Action => ({
-  type: NOTIFICATION,
-  payload
+    type: types.NOTIFICATION,
+    payload
 })
