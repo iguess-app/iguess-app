7f97c98bb3bf9a2f5d7c90bfd454f7b21f679548
Julio Xavier <julioxavierr@gmail.com>
2018-03-26 20:53:38 -0300

    Initial scene styled components

---
+++ src/components/Scene.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Name = styled.Text`
+  color: #000;
+  font-size: 30;
+  font-weight: bold;
+`;
+
+export const View = styled.View`
+  flex: 1;
+  background-color: #fff;
+`;
