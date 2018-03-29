734c50f1b9eb4e1710140f374fb5cf177b87a377
Julio Xavier <julioxavierr@gmail.com>
2018-03-28 22:26:47 -0300

    Initial styled component with team image and name

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,21 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
+import arsenal from '@assets/images/arsenal.png';
 
 const GameCard = props => {
   const { name } = props;
 
   return (
     <Card>
-      <CardName>{name}</CardName>
+      <Team name="Arsenal" image={arsenal} />
     </Card>
   );
 };
 
+const Team = props => {
+  const { name, image } = props;
+
+  return (
+    <TeamView>
+      <TeamImage source={image} />
+      <TeamName>{name}</TeamName>
+    </TeamView>
+  );
+};
+
+const TeamView = styled.View`
+  margin-horizontal: 16;
+  margin-top: 40;
+`;
+
+const TeamName = styled.Text`
+  opacity: 0.4;
+  font-size: 12;
+  color: #4d6980;
+`;
+
+const TeamImage = styled.Image`
+  width: 44;
+  height: 53;
+`;
+
 const Card = styled.View`
+  flex-direction: row;
   width: 312;
   height: 144;
   margin-bottom: 27;
-  marginhorizontal: 32;
+  margin-horizontal: 32;
   border-color: rgba(149, 177, 205, 0.25);
   border-radius: 4;
   border-width: 1;
