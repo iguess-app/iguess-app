c803065222b546e0dd6fba9e0dedc60067f801fb
Julio Xavier <julioxavierr@gmail.com>
2018-04-03 07:48:47 -0300

    Add close button on Settings

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { SceneWrapper, ScrollWrapper, Name } from '@components/Scene';
+import styled from 'styled-components';
+import close from '@assets/images/close-settings.png';
+import { SETTINGS_TEXT_COLOR } from '@theme';
 
 const Settings = props => {
   return (
     <SceneWrapper>
       <ScrollWrapper>
-        <Name>Settings</Name>
+        <Close />
       </ScrollWrapper>
     </SceneWrapper>
   );
 };
+
+const Close = styled.Image.attrs({
+  source: close,
+})`
+  width: 16;
+  height: 16;
+  margin-left: 32;
+`;
 
 export default Settings;
