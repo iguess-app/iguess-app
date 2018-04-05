02e9a5957f5b6fb0b3a4ba64faf8af2bc1aaf0c8
Julio Xavier <julioxavierr@gmail.com>
2018-04-05 13:47:27 -0300

    Touchable Select Line on Navigation

--- src/containers/Lines.js
+++ src/containers/Lines.js
@@ -9,6 +9,8 @@ import SelectedLine from '@components/SelectedLine';
 import { SceneWrapper, ScrollWrapper } from '@components/Scene';
 import styled from 'styled-components';
 import SettingsButton from '@components/SettingsButton';
+import { SELECT_LINE_PRIMARY_TEXT } from '@theme';
+import chevronDown from '@assets/images/chevron-down.png';
 
 class Lines extends Component {
   constructor(props) {
@@ -37,14 +39,17 @@ class Lines extends Component {
   render() {
     const { games, swipe } = this.props;
 
+    const NavigationSelectedLine = this.state.selectedOpacity < 0.1 ? 1 : 0;
+
     return (
       <SceneWrapper>
         <Navigation>
           <SettingsButton onPress={() => swipe()} />
+          <NavSelectedLine />
         </Navigation>
         <ScrollWrapper
           onScroll={this._changeSelectedLineOpacity.bind(this)}
-          scrollEventThrottle={16}
+          scrollEventThrottle={8}
         >
           <SelectedLine
             season="2018"
@@ -59,6 +64,15 @@ class Lines extends Component {
   }
 }
 
+const NavSelectedLine = () => {
+  return (
+    <NavTouchable>
+      <NavText>Russian World Cup</NavText>
+      <Chevron />
+    </NavTouchable>
+  );
+};
+
 function mapStateToProps(state) {
   return {
     games: gamesSelectors.getGames(state),
@@ -67,7 +81,28 @@ function mapStateToProps(state) {
 
 const Navigation = styled.View`
   flex-direction: row;
-  flex: 0.05;
+  flex: 0.08;
+`;
+
+const NavTouchable = styled.TouchableOpacity`
+  flex-direction: row;
+  margin-top: 2;
+  margin-left: 105;
+`;
+
+const NavText = styled.Text`
+  font-size: 16;
+  font-weight: bold;
+  color: ${SELECT_LINE_PRIMARY_TEXT};
+  margin-right: 16;
+`;
+
+const Chevron = styled.Image.attrs({
+  source: chevronDown,
+})`
+  width: 14;
+  resize-mode: contain;
+  margin-top: 4;
 `;
 
 export default connect(mapStateToProps)(Lines);
