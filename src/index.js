63b9134745652ae97a5646b4b78ea039914eabf8
Julio Xavier <julioxavierr@gmail.com>
2018-04-06 12:16:01 -0300

    Changing navigation architecture

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import createStore from '@store/create'
-import Router from '@scenes'
+import React from 'react';
+import Lines from '@containers/Lines';
+import Settings from '@containers/Settings';
+import LaunchContainer from '@containers/Launch';
+import { Router, Stack, Scene } from 'react-native-router-flux';
+import { Provider } from 'react-redux';
+import createStore from '@store/create';
+
+const Routes = () => {
+  return (
+    <Router>
+      <Stack key="root">
+        <Scene key="launch" component={LaunchContainer} hideNavBar={true} />
+        <Scene key="lines" component={Lines} hideNavBar={true} />
+        <Scene key="settings" component={Settings} hideNavBar={true} />
+      </Stack>
+    </Router>
+  );
+};
 
 export default () => (
-    <Provider store={createStore()}>
-      <Router />
-    </Provider>
-)
+  <Provider store={createStore()}>
+    <Routes />
+  </Provider>
+);
--- src/scene/index.js
+++
@@ -1,7 +0,0 @@
-/* @flow */
-
-import React, { Element } from 'react';
-import { Router } from 'react-native-router-flux';
-import scenes from './app';
-
-export default (): Element => <Router scenes={scenes} />;
