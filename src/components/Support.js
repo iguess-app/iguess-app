e5aa9504e2f5695fc3bbce5baae56ead7ce4a378
Julio Xavier <julioxavierr@gmail.com>
2018-04-10 08:45:13 -0300

    Subtext on Support page

--- src/components/Support.js
+++ src/components/Support.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { SceneWrapper } from '@components/Scene';
 import SecondaryScene from '@components/SecondaryScene';
+import styled from 'styled-components';
+import { SUBTEXT_SECONDARY_SCENE } from '../theme';
 
 const Support = () => {
   return (
@@ -9,8 +11,20 @@ const Support = () => {
         title="Support"
         description="Fale com alguém do nosso time ou contate-nos nas redes sociais."
       />
+      <SubText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend
+        vitae ipsum vehicula malesuada. Ut scelerisque nunc non consequat
+        ultricies.
+      </SubText>
     </SceneWrapper>
   );
 };
 
+const SubText = styled.Text`
+  font-size: 14;
+  color: ${SUBTEXT_SECONDARY_SCENE}
+  margin-top: 10;
+  padding-right: 32;
+  padding-left: 32;
+`;
 export default Support;
