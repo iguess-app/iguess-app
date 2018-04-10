2fad690000dff2ced21c30b095d0c7a7c6d10eda
Julio Xavier <julioxavierr@gmail.com>
2018-04-10 18:19:45 -0300

    First Support option button

--- src/components/Support.js
+++ src/components/Support.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { SceneWrapper } from '@components/Scene';
 import { Header, Content, SceneDescription } from '@components/SecondaryScene';
 import styled from 'styled-components';
+import { TEXT_SECONDARY_SCENE } from '@theme';
+import chevronRight from '@assets/images/chevron-right.png';
+import mail from '@assets/images/mail.png';
 
 const Support = () => {
   return (
@@ -15,8 +18,49 @@ const Support = () => {
         vitae ipsum vehicula malesuada. Ut scelerisque nunc non consequat
         ultricies.
       </Content>
+      <Option icon={mail} text="support@iguess.com" />
     </SceneWrapper>
   );
 };
+
+const Option = props => {
+  const { icon, text } = props;
+
+  return (
+    <TouchableWrapper>
+      <OptionIcon source={icon} />
+      <OptionText>{text}</OptionText>
+      <Chevron />
+    </TouchableWrapper>
+  );
+};
+
+const TouchableWrapper = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  margin-left: 32;
+  margin-right: 32;
+  margin-bottom: 40;
+`;
+
+const OptionIcon = styled.Image`
+  width: 32;
+  margin-right: 20;
+  resize-mode: contain;
+`;
+
+const OptionText = styled.Text`
+  font-size: 16;
+  width: 240;
+  color: ${TEXT_SECONDARY_SCENE};
+`;
+
+const Chevron = styled.Image.attrs({
+  source: chevronRight,
+})`
+  align-content: flex-end;
+  height: 16;
+  width: 10;
+`;
 
 export default Support;
