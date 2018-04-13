540fa511c4607321e687060d138a7f71d9454447
Julio Xavier <julioxavierr@gmail.com>
2018-04-13 10:13:29 -0300

    Refactoring Settings

--- src/scenes/Settings.js
+++ src/scenes/Settings.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { SceneWrapper } from '@components/Scene';
+import { TouchableOpacity } from 'react-native';
 import styled from 'styled-components';
-import close from '@assets/images/close-settings.png';
-import { SETTINGS_TEXT_COLOR, SETTINGS_BORDER_COLOR } from '@theme';
+import { SceneWrapper } from '@components/Scene';
+import { Actions } from 'react-native-router-flux';
 import conversation from '@assets/images/conversation.png';
+import close from '@assets/images/close-settings.png';
 import blog from '@assets/images/blog.png';
 import store from '@assets/images/store.png';
 import exit from '@assets/images/exit.png';
-import { TouchableOpacity } from 'react-native';
-import { Actions } from 'react-native-router-flux';
+import { SETTINGS_TEXT_COLOR, SETTINGS_BORDER_COLOR } from '@theme';
 
 const Settings = props => {
   const { swipe } = props;
@@ -70,28 +70,28 @@ const Close = props => {
 const CloseImage = styled.Image.attrs({
   source: close,
 })`
-  margin-top: 32;
   width: 16;
   height: 16;
   margin-left: 32;
+  margin-top: 32;
 `;
 
 const Title = styled.Text`
-  margin-top: 40;
-  margin-left: 32;
-  margin-bottom: 24;
   font-size: 24;
   font-weight: bold;
   color: ${SETTINGS_TEXT_COLOR};
+  margin-top: 40;
+  margin-left: 32;
+  margin-bottom: 24;
 `;
 
 const TouchableView = styled.TouchableOpacity`
   flex-direction: row;
+  height: 72;
   border-top = solid;
   border-color: ${SETTINGS_BORDER_COLOR};
   border-top-width: 1;
   border-bottom-width: 1;
-  height: 72;
 `;
 
 const Icon = styled.Image`
@@ -105,9 +105,9 @@ const Icon = styled.Image`
 const CustomText = styled.Text`
   font-size: 14;
   font-weight: bold;
+  color: ${SETTINGS_TEXT_COLOR};
   margin-left: 20;
   margin-top: 28;
-  color: ${SETTINGS_TEXT_COLOR};
 `;
 
 const LogoutView = styled.TouchableOpacity`
