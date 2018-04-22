4b4484adb686a74d867e4980635760ad86187c8c
Julio Xavier <julioxavierr@gmail.com>
2018-04-22 15:26:44 -0300

    Update SignIn inputs to use material kit

--- src/scenes/SignIn.js
+++ src/scenes/SignIn.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { InputSceneWrapper } from '@components/Scene';
 import { NavBar } from '@components/Scene';
 import { MainButton } from '@components/Button';
+import Input from '@components/Input';
 import styled from 'styled-components';
 import { INPUT_BORDER_COLOR } from '@theme';
 
@@ -15,11 +16,11 @@ class SignUp extends Component {
       <InputSceneWrapper>
         <NavBar title="Sign in" />
         <Wrapper>
-          <Input
+          <TextInput
             placeholder="Username or e-mail"
             onChangeText={text => this.setState({ text })}
           />
-          <Input
+          <TextInput
             placeholder="My password"
             onChangeText={text => this.setState({ text })}
           />
@@ -37,16 +38,14 @@ const Wrapper = styled.View`
   margin-right: 32;
 `;
 
-const Input = styled.TextInput`
-  height: 40;
-  border-bottom-width: 1;
-  border-color: ${INPUT_BORDER_COLOR};
+const TextInput = styled(Input)`
   margin-top: 48;
 `;
 
 const ButtonView = styled.View`
   margin-top: 32;
   margin-bottom: 16;
+  align-self: center;
 `;
 
 export default SignUp;
