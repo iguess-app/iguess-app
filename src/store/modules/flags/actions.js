ff8ee9859623959fb879ff5c4f93cb39c80adc8f
Julio Xavier <julioxavierr@gmail.com>
2018-05-07 16:44:23 -0300

    Removing unused action

--- src/store/modules/flags/actions.js
+++ src/store/modules/flags/actions.js
@@ -1,15 +1,6 @@
-/* @flow */
-
 import * as types from './actionTypes';
-
-type Action = Object<{ type: string, payload: boolean }>;
 
 export const swipe = (payload): Action => ({
   type: types.ACTIVE_SCREEN_SWIPER,
-  payload,
-});
-
-export const showNotification = (payload): Action => ({
-  type: types.NOTIFICATION,
   payload,
 });
