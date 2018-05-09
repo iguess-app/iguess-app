ee618f6de451975e5853207cfc6c6385e8b402da
Julio Xavier <julioxavierr@gmail.com>
2018-05-09 18:46:20 -0300

    Basic title for game day on GameList

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import { connect } from 'react-redux';
 import * as gamesActions from '@redux/games/actions';
 import * as gamesSelectors from '@redux/games/reducer';
-import { SCENE_BACKGROUND_COLOR } from '../theme';
+import { SCENE_BACKGROUND_COLOR, CARD_LIST_TITLE_COLOR, WIDTH_REL, HEIGHT_REL } from '@theme';
 
 class GameList extends Component {
 
@@ -19,6 +19,18 @@ class GameList extends Component {
   render() {
     return (
       <Wrapper>
+        <Title first>HOJE</Title>
+        <List
+          data={this.props.games}
+          renderItem={({ item }) => (
+            <GameCard
+              id={item.key}
+              HomeGuess={item.HomeGuess}
+              AwayGuess={item.AwayGuess}
+            />
+          )}
+        />
+        <Title>AMANHÃ</Title>
         <List
           data={this.props.games}
           renderItem={({ item }) => (
@@ -35,11 +47,11 @@ class GameList extends Component {
 }
 
 const List = styled.FlatList`
-  margin-top: 16;
+  margin-top: ${16*HEIGHT_REL};
 `;
 
 export const Wrapper = styled.ScrollView`
-  margin-top: 60;
+  margin-top: ${60*HEIGHT_REL};
   flex: 1;
   background-color: ${SCENE_BACKGROUND_COLOR};
 `;
@@ -49,5 +61,13 @@ const mapStateToProps = state => {
     games: gamesSelectors.getGames(state),
   };
 }
+
+const Title = styled.Text`
+  margin-top: ${props => props.first ? 20*HEIGHT_REL : 0};
+  font-size: 28;
+  font-weight: bold;
+  color: ${CARD_LIST_TITLE_COLOR};
+  margin-left: ${32*WIDTH_REL};
+`
 
 export default connect(mapStateToProps)(GameList);
--- src/theme/index.js
+++ src/theme/index.js
@@ -27,6 +27,7 @@ export const INPUT_ERROR_COLOR = '#EE4042'; // red
 export const SIGN_UP_TERMS_COLOR = '#4D6980'; // darker-gray
 export const LOADING_TITLE_COLOR = SETTINGS_TEXT_COLOR; //purple
 export const LOADING_SUBTITLE_COLOR = GUESS_DEFAULT_TEXT_COLOR; // darker-gray
+export const CARD_LIST_TITLE_COLOR = '#5435D4'; // purple
 
 // BACKGROUND
 
