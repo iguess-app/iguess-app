5bf4ad45d55d15ffba385f7674bc34dc17bc7d29
Julio Xavier <julioxavierr@gmail.com>
2018-05-30 11:42:53 -0300

    Handling back button

--- src/scenes/Core.js
+++ src/scenes/Core.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BackHandler } from 'react-native';
 import Lines from '@scenes/Lines';
 import Settings from '@scenes/Settings';
 import { setStatusBarStyle } from '@helpers';
@@ -6,8 +7,18 @@ import Swiper from 'react-native-swiper';
 import { connect } from 'react-redux';
 import { swipe } from '@redux/flags/actions';
 import styled from 'styled-components';
+import { Actions } from 'react-native-router-flux';
 
 class Core extends Component<void, void, void> {
+  componentWillMount() {
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      if (Actions.currentScene === 'core') {
+        BackHandler.exitApp();
+        return true;
+      }
+    });
+  }
+
   render() {
     const { dispatch, activeSwiperScreen } = this.props;
 
