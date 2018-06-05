a98c0b6257b46bafa5ac07e7d4a2a1871d81c293
Lucas Alves <lucashalves@live.com>
2018-06-05 10:57:27 -0300

    cutting off all the pontuation words at text

--- src/i18n/locales/en.js
+++ src/i18n/locales/en.js
@@ -73,26 +73,26 @@ export default {
   errorPredictionButton: 'Try again',
 
   howItWorksTitle: 'How the app works',
-  howItWorksDescription1: 'Any doubt about the pontuation?',
+  howItWorksDescription1: 'Any doubt about how to score?',
   howItWorksContent1:
-    'So here we go, we will explain how we get at pontuation with your predict.',
+    'So here we go, we will explain how we get the score with your predict.',
   howItWorksContent2:
-    "To score it is necessary hit the winner of the match, so a match's pontuation is the subtract between the top pontuation and the diff of goals.",
+    "To score it is necessary hit the winner of the match, so a match's score points is the subtract between the top score point and the diff of goals.",
   howItWorksContent3: 'Who hit exactly the scoreboard gains 10 points.',
-  howItWorksBoardTitle: 'MP = TP - | (HTS) - (ATS) |',
+  howItWorksBoardTitle: 'MSP = TSP - | (HTS) - (ATS) |',
   howItWorksBoardContent:
-    'MP - Match Pontuation\nTP - Top Pontuation\nHTS - Home Team Subtract Predict/Score\nATS - Away Team Subtract Predict/Score',
+    'MSP - Match Score Point\nTSP - Top Score Point\nHTS - Home Team Subtract Predict/Score\nATS - Away Team Subtract Predict/Score',
   howItWorksDescription2: 'Examples:',
   howItWorksContent4:
-    'Predict: England 1 x 2 Argentina\nResult: England 1 x 4 Argentina\nPontuation = 10 - | (1 - 1) + (2 - 4) | = 8',
+    'Predict: England 1 x 2 Argentina\nResult: England 1 x 4 Argentina\nMatch Score Point = 10 - | (1 - 1) + (2 - 4) | = 8',
   howItWorksContent5:
-    'The min pontuation for someone who hit the winner it is 2 points',
+    'The min score point for someone who hit the winner it is 2 points',
   howItWorksContent6: 'In case of draw:',
   howItWorksContent7:
-    'Predict: Brazil 0 x 0 Netherlands\nResult: Brazil 2 x 2 Netherlands\nPontuation = 10 - | (0 - 2) + (0 - 2) | = 6',
+    'Predict: Brazil 0 x 0 Netherlands\nResult: Brazil 2 x 2 Netherlands\nMatch Score Point = 10 - | (0 - 2) + (0 - 2) | = 6',
   howItWorksContent8: 'In case of error:',
   howItWorksContent9:
-    'Predict: Uruguay 0 x 2 Portugal\nResult: Uruguay 2 x 1 Portugal\nPontuation = 0',
+    'Predict: Uruguay 0 x 2 Portugal\nResult: Uruguay 2 x 1 Portugal\nMatch Score Point = 0',
   howItWorksContent10:
     '1h before the match starts the predicts to the match are blocked.',
 };
